3731e7630ce18a968b0f96950268e1dacb1c5c98
EvanKelley <sionnach1993@gmail.com>
2023-05-11 10:55:34 -0500

    add 5

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-let arrayOfUsers;
+let arrayOfUsers = [];
 
-const getUsers = () => 
-{
-  fetch('https://randomuser.me/api/')
-    .then(res => res.json())
-    .then(users => arrayOfUsers = users)
-}
+const getUsers = (numUsers) => {
+  const userPromises = [];
 
-const consoleUsers = () =>
-{
-  console.log(arrayOfUsers)
-}
+  for (let i = 0; i < numUsers; i++) {
+    userPromises.push(
+      fetch("https://randomuser.me/api/").then((res) => res.json())
+    );
+  }
+
+  Promise.all(userPromises).then((users) => {
+    arrayOfUsers = users;
+    console.log(arrayOfUsers);
+  });
+};
+
+getUsers(5);
