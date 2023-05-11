8a59d3690d6f433abe986e5c0f2f7d7bbeabb9ac
EvanKelley <sionnach1993@gmail.com>
2023-05-11 12:45:55 -0500

    lists names

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
-let arrayOfUsers = [];
+let arrayOfUsers
+const list = document.getElementById("usersList")
+const getUsers = () => {
+  fetch("https://randomuser.me/api/?results=5").then((res) => res.json())
+  // .then(data => arrayOfUsers.push(data.results[0]))
+  .then(users => users.results.forEach(user => {
+    // console.log(user)
+    const li = document.createElement("li")
+    // const userImg = document.createElement("img")
+    // userImg.src = user.picture.medium
+    //userListItem.innerHTML = user.name.first
+    //userListItem.appendChild(userImg)
+    //usersList.appendChild(userListItem)
+    const p = document.createElement('p');
+    let info = `
+        Name: ${user.name.first} ${user.name.last}
+    `
+    p.innerText = info;
+    li.appendChild(p);
+    list.appendChild(li);
+  }))
+  // console.log(arrayOfUsers)
+}
 
-const getUsers = (numUsers) => {
-  const userPromises = [];
+window.onload = getUsers()
 
-  for (let i = 0; i < numUsers; i++) {
-    userPromises.push(
-      fetch("https://randomuser.me/api/").then((res) => res.json())
-    );
-  }
+// const getUsers = (numUsers) => {
+//   const userPromises = [];
 
-  Promise.all(userPromises).then((users) => {
-    arrayOfUsers = users;
-    console.log(arrayOfUsers);
-  });
-};
+//   for (let i = 0; i < numUsers; i++) {
+//     userPromises.push(
+//       fetch("https://randomuser.me/api/").then((res) => res.json())
+//     );
+//   }
 
-getUsers(5);
+//   Promise.all(userPromises).then((users) => {
+//     arrayOfUsers = users;
+//     console.log(arrayOfUsers);
+//   });
+// };
+// getUsers(5);
